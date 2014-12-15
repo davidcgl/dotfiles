c978ec80c7988007eed1aea258b50a4148e79768
David Chen <cgl.david@gmail.com>
2014-12-14 17:20:21 -0800

    Added install.sh

--- mac/install.sh
+++ mac/install.sh
@@ -1,14 +1,15 @@
 #! /bin/zsh
-# Create symlinks to all dotfiles in this directory.
 
 SRC_DIR=`pwd`
 
+# Create symlinks to all dotfiles in this directory.
 for FILE in .*; do
     SRC_FILE=$SRC_DIR/$FILE
     DST_FILE=$HOME/$FILE
     if [[ -f $DST_FILE ]]; then
         rm $DST_FILE
     fi
-    ln -s $SRC_FILE $DST_FILE 
-    echo 'ln -s' $SRC_FILE $DST_FILE
+    CMD="ln -s $SRC_FILE $DST_FILE"
+    echo $CMD
+    eval $CMD
 done
