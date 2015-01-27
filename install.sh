75cf8eb9fc19579ce390b818449ee35da122310c
David Chen <cgl.david@gmail.com>
2015-01-26 21:16:42 -0800

    Updates conditional to check for file existence

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
-#! /bin/zsh
+#!/bin/zsh
 
 SRC_DIR=`pwd`
+FILES=(.zshrc .vimrc .gitconfig .tmux.conf)
 
 # Create symlinks to all dotfiles in this directory.
-for FILE in .*; do
+for FILE in ${FILES[*]}; do
     SRC_FILE=$SRC_DIR/$FILE
     DST_FILE=$HOME/$FILE
-    if [[ -f $DST_FILE ]]; then
+    if [[ -h $DST_FILE || -e $DST_FILE ]]; then
         rm $DST_FILE
     fi
     CMD="ln -s $SRC_FILE $DST_FILE"
