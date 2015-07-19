b8233f3fb176f95d79cd78b2ced70f845be3ee53
David Chen <cgl.david@gmail.com>
2015-07-19 10:11:31 -0700

    Move actual dotfiles into dotfiles/ folder and update install script

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/zsh
 
-SRC_DIR=`pwd`
-FILES=(.zshrc .vimrc .gitconfig .tmux.conf)
+ROOT_DIR=`pwd`
+SRC_DIR=dotfiles
 
 # Create symlinks to all dotfiles in this directory.
-for FILE in ${FILES[*]}; do
-    SRC_FILE=$SRC_DIR/$FILE
-    DST_FILE=$HOME/$FILE
+for FILE in `ls $SRC_DIR`; do
+    SRC_FILE=$ROOT_DIR/$SRC_DIR/$FILE
+    DST_FILE=$HOME/.$FILE
     if [[ -h $DST_FILE || -e $DST_FILE ]]; then
         rm $DST_FILE
     fi
