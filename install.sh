a59ebba6a5bc4ef830aab3b5df58bf77f80909ba
David Chen <git@davidchen.io>
2023-06-13 12:59:28 -0700

    Updates for conda

--- install.sh
+++ install.sh
@@ -5,13 +5,22 @@ run() {
   eval "$1"
 }
 
-ROOT_DIR=$(pwd)
-SRC_DIR=dotfiles
+# Directory of this script.
+ROOT_DIR=$(dirname ${0:A})
 
 # Copy dotfiles to home directory.
-for FILE in gitconfig gitignore ignore tmux.conf vimrc zpreztorc zshrc zshrc_common zshrc_home; do
-  SRC_FILE=${ROOT_DIR}/${SRC_DIR}/${FILE}
+for FILE in condarc gitconfig gitignore ignore tmux.conf vimrc zpreztorc zshrc zshrc_common zshrc_home; do
+  SRC_FILE=${ROOT_DIR}/dotfiles/${FILE}
   DST_FILE=${HOME}/.${FILE}
+  run "rm -f ${DST_FILE}"
+  run "ln -s ${SRC_FILE} ${DST_FILE}"
+done
+
+# Copy scripts to ~/bin.
+run "mkdir -p ${HOME}/bin/"
+for FILE in sync-docs conda-auto-env; do
+  SRC_FILE=${ROOT_DIR}/scripts/${FILE}
+  DST_FILE=${HOME}/bin/.${FILE}
   run "rm -f ${DST_FILE}"
   run "ln -s ${SRC_FILE} ${DST_FILE}"
 done
